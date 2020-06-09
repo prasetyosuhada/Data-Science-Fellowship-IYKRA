3d54cc07de7a5cf00f8529c04f8857e81b1e9046
prasetyosuhada <61892969+prasetyosuhada@users.noreply.github.com>
2020-06-09 14:27:02 +0700

    Update No 1.sql

--- Practice Case SQL/No 1.sql
+++ Practice Case SQL/No 1.sql
@@ -1,4 +1,2 @@
 select count(title) as recommendation_film from film
 where description like '%Astronaut%' 
-
-![Task](https://user-images.githubusercontent.com/61892969/84118282-f86fa180-aa5c-11ea-90fe-d795e017c4d2.PNG)
